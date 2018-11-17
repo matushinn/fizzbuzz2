8f563701054bd812fab04aae94c6bd41f529add6
shotaro-ooe <shoooe97@gmail.com>
2018-11-17 15:07:37 +0900

    リファクタリング：意図が通じやすくなるから

--- fizbazz.py
+++ fizbazz.py
@@ -1,13 +1,12 @@
 number = int(input("一つの自然数を入れてね:"))
 
 if number % 15 == 0:
-    output = "fizzbuzz"
+
+    print("fizzbuzz")
 elif number % 3 == 0:
-    output = "Fizz"
+    print("Fizz")
 elif number % 5 == 0:
-    output = "Buzz"
+    print("Buzz")
 
 else:
-    output = str(number)
-
-print(output)
+    print(str(number))
