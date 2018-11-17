616bbc265154e27117af7762240cd640a0075ab6
shotaro-ooe <shoooe97@gmail.com>
2018-11-17 14:54:52 +0900

    fizzbazz完成

--- fizbazz.py
+++ fizbazz.py
@@ -1,9 +1,12 @@
 number = int(input("一つの自然数を入れてね:"))
 
-if number % 3 == 0:
+if number % 15 == 0:
+    output = "fizzbuzz"
+elif number % 3 == 0:
     output = "Fizz"
 elif number % 5 == 0:
     output = "Buzz"
+
 else:
     output = str(number)
 
